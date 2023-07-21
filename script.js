a864a2cfb161d01eee0e0b441813ff5c9724068d
Gauri <gaurivarshney883@gmail.com>
2023-07-21 22:40:50 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
-
-let string ="";
-let arr = Array.from(buttons);
-
-arr.forEach(buttons =>{
-    buttons.addEventListener('click', (e)=>{
-        if(e.target.innerHTML == '=')
-        {
-            string = eval(string);
-            input.value = string;
-        }else if(e.target.innerHTML== 'AC')
-        {
-            string = "";
-            input.value = string;
-        }else if(e.target.innerHTML=='DEL')
-        {
-            string = string.substring(0, string.length-1);
-            input.value =string;
-        }else{
-            string+= e.target.innerHTML;
-            input.value =string;
-        }
-    })
-})
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
